3e2e4966f962af826a85f5424f11e531bb285ac0
yuya-3110 <ysuu45143745@gmail.com>
2024-10-05 16:32:40 +0900

    ItemTableViewCellの不要なコメントを削除

--- MyShoppingList/ItemTableViewCell.swift
+++ MyShoppingList/ItemTableViewCell.swift
@@ -10,27 +10,27 @@ import UIKit
 
 // カスタムテーブルビューセルクラス
 class ItemTableViewCell: UITableViewCell {
-    @IBOutlet weak var checkImageView: UIImageView!  // チェックマークを表示するための画像ビュー
-    @IBOutlet weak var purchaseDateLabel: UILabel!  // 購入日を表示するためのラベル
-    @IBOutlet weak var categoryLabel: UILabel!  // カテゴリー名を表示するためのラベル
-    @IBOutlet weak var nameLabel: UILabel!  // アイテム名を表示するためのラベル
+    @IBOutlet weak var checkImageView: UIImageView!
+    @IBOutlet weak var purchaseDateLabel: UILabel!
+    @IBOutlet weak var categoryLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
 
-    // セルの内容をアイテムデータで設定するメソッド/
+    // アイテムデータを用いてセルの内容を設定
     func configure(item: TableViewController.Item) {
-        if item.isChecked {  // アイテムがチェックされている場合、チェックマーク画像を設定
-            checkImageView.image = UIImage(named: "check")  // "check"という名前の画像を表示
-        } else {
-            checkImageView.image = nil  // チェックされていない場合、画像を表示しない
-        }
-        if let purchaseDate = item.purchaseDate {  // 購入日が設定されている場合
-            let dateFormatter = DateFormatter()
-            dateFormatter.dateFormat = "M/d"  // 月/日形式に設定
-            purchaseDateLabel.text = dateFormatter.string(from: purchaseDate)  // 日付を文字列に変換してラベルに設定
-        } else {
-            purchaseDateLabel.text = "No Date"  // 購入日が未設定の場合の表示
-        }
-        categoryLabel.text = item.category  // カテゴリー名をラベルに設定
-        nameLabel.text = item.name  // アイテム名をラベルに設定
+        // チェックマークの表示切り替え
+        checkImageView.image = item.isChecked ? UIImage(named: "check") : nil
 
+        // 購入日の表示設定
+        if let purchaseDate = item.purchaseDate {
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateFormat = "M/d"
+            purchaseDateLabel.text = dateFormatter.string(from: purchaseDate)
+        } else {
+            purchaseDateLabel.text = "No Date"
+        }
+
+        // カテゴリと名前の表示
+        categoryLabel.text = item.category
+        nameLabel.text = item.name
     }
 }
